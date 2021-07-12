69c88d415449bdc83d043aa5c84581da40a838e3
Belen <beleneines8@gmail.com>
2021-07-12 12:54:01 +0200

    'Name_as_param'

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,20 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
-    {
-    //    return $this->render('conference/index.html.twig', [
-    //        'controller_name' => 'ConferenceController',
-    //    ]);
-        return new Response (<<<EOF
+    public function index(Request $request): Response {
+        $greet = '';
+        if($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1> Hello </h1>', htmlspecialchars($name));
+        }
+
+        return new Response(<<<EOF
             <html>
                 <body>
+                    $greet
                     <img src="/images/under-construction.gif" />
                 </body>
             </html>
